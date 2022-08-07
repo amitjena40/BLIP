e2eb17d1240e93649752995db59a619499e9c792
Amit Kumar Jena <35566715+amitjena40@users.noreply.github.com>
2022-08-07 15:37:40 +0530

    Update vit.py

--- models/vit.py
+++ models/vit.py
@@ -53,7 +53,7 @@ class Attention(nn.Module):
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
         self.attn_gradients = None
-        self.attention_map = torch.tensor([])
+        self.attention_map = None
         
     def save_attn_gradients(self, attn_gradients):
         self.attn_gradients = attn_gradients
@@ -67,17 +67,16 @@ class Attention(nn.Module):
     def get_attention_map(self):
         return self.attention_map
     
-    def forward(self, x, register_hook=False):
+    def forward(self, x, register_hook_check=False):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
 
-        attn = torch.tensor([])
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
                 
-        if register_hook:
+        if register_hook_check:
             self.save_attention_map(attn)
             attn.register_hook(self.save_attn_gradients)        
 
